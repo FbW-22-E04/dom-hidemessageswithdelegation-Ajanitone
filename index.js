fc83b7117fbce7892aa415bed4d35c03b6138208
Ajanitone <ajanitone@freenet.de>
2022-09-24 17:52:05 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+const container = document.querySelector("#container");
+const pane = document.querySelectorAll(".pane");
+
+container.addEventListener("click", (e) => {
+  if (e.target.className === "remove-button") {
+    e.target.parentElement.style.display = "none";
+  }
+});
